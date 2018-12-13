289426369c6297d8aa88a05a69ce1388f7fd0719
Josiah Ethington <josiah.ethington@wsu.edu>
2018-12-12 20:39:11 -0800

    Working duty time calculator with test file.

---
+++ WinFormDutyCalc/WinFormDutyCalc/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WinFormDutyCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileNameBox = new System.Windows.Forms.TextBox();
+            this.hoursThisQuarterLabel = new System.Windows.Forms.Label();
+            this.hoursFlownLastLabel = new System.Windows.Forms.Label();
+            this.hoursLeftThisQuarterLabel = new System.Windows.Forms.Label();
+            this.hoursAvailableNextQuarterLabel = new System.Windows.Forms.Label();
+            this.hoursLeftThisYearLabel = new System.Windows.Forms.Label();
+            this.requiredRestLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // fileNameBox
+            // 
+            this.fileNameBox.AccessibleName = "File name";
+            this.fileNameBox.Location = new System.Drawing.Point(57, 75);
+            this.fileNameBox.Name = "fileNameBox";
+            this.fileNameBox.Size = new System.Drawing.Size(100, 20);
+            this.fileNameBox.TabIndex = 1;
+            this.fileNameBox.Text = "test.csv";
+            // 
+            // hoursThisQuarterLabel
+            // 
+            this.hoursThisQuarterLabel.AutoSize = true;
+            this.hoursThisQuarterLabel.Location = new System.Drawing.Point(537, 43);
+            this.hoursThisQuarterLabel.Name = "hoursThisQuarterLabel";
+            this.hoursThisQuarterLabel.Size = new System.Drawing.Size(124, 13);
+            this.hoursThisQuarterLabel.TabIndex = 2;
+            this.hoursThisQuarterLabel.Text = "Hours flown this quarter: ";
+            // 
+            // hoursFlownLastLabel
+            // 
+            this.hoursFlownLastLabel.AutoSize = true;
+            this.hoursFlownLastLabel.Location = new System.Drawing.Point(537, 68);
+            this.hoursFlownLastLabel.Name = "hoursFlownLastLabel";
+            this.hoursFlownLastLabel.Size = new System.Drawing.Size(124, 13);
+            this.hoursFlownLastLabel.TabIndex = 3;
+            this.hoursFlownLastLabel.Text = "Hours flown last quarter: ";
+            // 
+            // hoursLeftThisQuarterLabel
+            // 
+            this.hoursLeftThisQuarterLabel.AutoSize = true;
+            this.hoursLeftThisQuarterLabel.Location = new System.Drawing.Point(537, 95);
+            this.hoursLeftThisQuarterLabel.Name = "hoursLeftThisQuarterLabel";
+            this.hoursLeftThisQuarterLabel.Size = new System.Drawing.Size(110, 13);
+            this.hoursLeftThisQuarterLabel.TabIndex = 4;
+            this.hoursLeftThisQuarterLabel.Text = "Hours left this quarter:";
+            // 
+            // hoursAvailableNextQuarterLabel
+            // 
+            this.hoursAvailableNextQuarterLabel.AutoSize = true;
+            this.hoursAvailableNextQuarterLabel.Location = new System.Drawing.Point(537, 139);
+            this.hoursAvailableNextQuarterLabel.Name = "hoursAvailableNextQuarterLabel";
+            this.hoursAvailableNextQuarterLabel.Size = new System.Drawing.Size(142, 13);
+            this.hoursAvailableNextQuarterLabel.TabIndex = 5;
+            this.hoursAvailableNextQuarterLabel.Text = "Hours available next quarter:";
+            // 
+            // hoursLeftThisYearLabel
+            // 
+            this.hoursLeftThisYearLabel.AutoSize = true;
+            this.hoursLeftThisYearLabel.Location = new System.Drawing.Point(537, 117);
+            this.hoursLeftThisYearLabel.Name = "hoursLeftThisYearLabel";
+            this.hoursLeftThisYearLabel.Size = new System.Drawing.Size(100, 13);
+            this.hoursLeftThisYearLabel.TabIndex = 6;
+            this.hoursLeftThisYearLabel.Text = "Hours left this year: ";
+            // 
+            // requiredRestLabel
+            // 
+            this.requiredRestLabel.AutoSize = true;
+            this.requiredRestLabel.Location = new System.Drawing.Point(276, 43);
+            this.requiredRestLabel.Name = "requiredRestLabel";
+            this.requiredRestLabel.Size = new System.Drawing.Size(167, 13);
+            this.requiredRestLabel.TabIndex = 7;
+            this.requiredRestLabel.Text = "Required Rest Period Remaining: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "FAR 315.269 Flight Time Regulations Calculator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.requiredRestLabel);
+            this.Controls.Add(this.hoursLeftThisYearLabel);
+            this.Controls.Add(this.hoursAvailableNextQuarterLabel);
+            this.Controls.Add(this.hoursLeftThisQuarterLabel);
+            this.Controls.Add(this.hoursFlownLastLabel);
+            this.Controls.Add(this.hoursThisQuarterLabel);
+            this.Controls.Add(this.fileNameBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox fileNameBox;
+        private System.Windows.Forms.Label hoursThisQuarterLabel;
+        private System.Windows.Forms.Label hoursFlownLastLabel;
+        private System.Windows.Forms.Label hoursLeftThisQuarterLabel;
+        private System.Windows.Forms.Label hoursAvailableNextQuarterLabel;
+        private System.Windows.Forms.Label hoursLeftThisYearLabel;
+        private System.Windows.Forms.Label requiredRestLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinFormDutyCalc/WinFormDutyCalc/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormDutyCalc
+{
+    public partial class Form1 : Form
+    {
+        double hoursThisyear = 0;
+        double hoursLastQuarter = 0;
+        double hoursThisQuarter = 0;
+        double hoursSinceLastFlight = -1;
+        DateTime now; 
+        int thisQuarter;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            hoursThisyear = 0;
+            hoursThisQuarter = 0;
+            hoursLastQuarter = 0;
+            hoursSinceLastFlight = -1;
+            now = System.DateTime.Now;
+            thisQuarter = FindQuarter(now);
+            string yourFile = fileNameBox.Text;
+            StreamReader readFile = new StreamReader(yourFile);
+            while (!readFile.EndOfStream)
+            {
+                string entry = readFile.ReadLine();
+                var separated = entry.Split(',');
+                DateTime start = Convert.ToDateTime(separated[0]+separated[1]);
+                DateTime end = Convert.ToDateTime(separated[0] + separated[2]);
+                string hoursAndMinutes = separated[3];
+                var hoursSplitter = hoursAndMinutes.Split(':');
+                double hours = (Double.Parse(hoursSplitter[0]) + Double.Parse(hoursSplitter[1])/60);
+                if (start.Year == now.Year)
+                {
+                    hoursThisyear += hours;
+                }
+                int quarter = FindQuarter(start);
+                if(quarter == thisQuarter)
+                {
+                    hoursThisQuarter += hours;
+                }else if(thisQuarter - quarter == 1 || thisQuarter - quarter == -3)
+                {
+                    hoursLastQuarter += hours;
+                }
+                if(hoursSinceLastFlight < 0 || (now - end).TotalHours < hoursSinceLastFlight)
+                {
+                    hoursSinceLastFlight = (now - end).TotalHours;
+                }
+            }
+            readFile.Close();
+            SetLabels();
+        }
+        private void SetLabels()
+        {
+            double hoursLeftInYear = 1400;
+            double hoursLeftInQuarter = 500;
+            double hoursNextQuarter = 500;
+            if(hoursSinceLastFlight > 12)
+            {
+                requiredRestLabel.Text = "Required Rest Period Remaining: " + "0";
+            }
+            else
+            {
+                requiredRestLabel.Text = "Required Rest Period Remaining: " + (12 -hoursSinceLastFlight);
+            }
+
+            hoursLeftInYear -= hoursThisyear;
+            if (hoursLastQuarter > 300)
+            {
+                hoursLeftInQuarter = 800 - (hoursLastQuarter + hoursThisQuarter);
+            }
+            else
+            {
+                hoursLeftInQuarter -= hoursThisQuarter;
+            }
+            if(hoursThisQuarter > 300)
+            {
+                hoursNextQuarter = 800 - hoursThisQuarter;
+            }
+
+            hoursThisQuarterLabel.Text = "Hours flown this quarter: " + hoursThisQuarter;
+            hoursFlownLastLabel.Text = "Hours flown last quarter: " + hoursLastQuarter;
+            hoursLeftThisQuarterLabel.Text = "Hours left this quarter: " + hoursLeftInQuarter;
+            hoursLeftThisYearLabel.Text = "Hours left this year: " + hoursLeftInYear;
+            hoursAvailableNextQuarterLabel.Text = "Hours available next quarter:" + hoursNextQuarter;
+
+
+        }
+
+        private int FindQuarter(DateTime date)
+        {
+            switch (date.Month)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    return 1;              
+                case 4:
+                case 5:
+                case 6:
+                    return 2;
+                case 7:
+                case 8:
+                case 9:
+                    return 3;
+                case 10:
+                case 11:
+                case 12:
+                    return 4;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
